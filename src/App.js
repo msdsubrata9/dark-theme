620c6e9ec193b11637600a4d141af487ed7f4685
Subrata Saha <msdsubrata9@gmail.com>
2024-12-10 20:03:07 +0530

    Dark theme implementation with Nth prime number to understand about useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import { findPrime } from "./utils/helper";
 
 function App() {
+  const [text, setText] = useState("");
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  console.log("rendering....");
+  const prime = useMemo(() => findPrime(text), [text]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <div
+          className={`p-2 m-4 w-96 h-96 border flex flex-col items-center ${
+            isDarkTheme
+              ? "bg-gray-800 text-white border-white"
+              : "bg-white text-black border-black"
+          }`}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <button
+              className="p-2 m-2 bg-green-400"
+              onClick={() => setIsDarkTheme(!isDarkTheme)}
+            >
+              toggle
+            </button>
+          </div>
+          <div>
+            <input
+              type="number"
+              className="border border-black text-black"
+              value={text}
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+            <div className={isDarkTheme ? "text-white" : "text-black"}>
+              Nth Prime:{prime}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
